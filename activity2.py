bf77b82ffd6e348a4621893f1f2b0de57dc884e3
Reda452 <redasakhi0@gmail.com>
2025-02-01 09:59:28 +0100

    new1

---
+++ activity2.py
@@ -0,0 +1,7 @@
+from datetime import date, time, datetime
+
+# calling the today
+#function of date class
+today = date.today()
+now = datetime.now()
+print(f"today's date is {today}\nCurrent date and time is {now}")
